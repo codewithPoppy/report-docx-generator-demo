398e5c150f45a1dc9bf5792caa0fef7da2067faf
helios0722 <90462652+helios0722@users.noreply.github.com>
2015-08-06 09:19:00 -0700

    Improved architecture - D:/Work/@/report-docx-generator-demo/src/logger.ts

--- src/logger.ts
+++ src/logger.ts
@@ -1,8 +1,1 @@
-const logger = class {
-  constructor(name: string) {}
-  info(log: string, obj?: any) { console.log(log, obj); };
-  error(err: string, obj?: any) { console.error(err, obj); };
-  debug(err: string, obj?: any) { console.debug(err, obj); };
-};
-
-export default logger;
+"Thu Aug 06 2015 09:19:00 GMT-0700 (Pacific Daylight Time)" 
