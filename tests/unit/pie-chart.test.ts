098f0dbe78a3a52968ee7fae1e13a47c19c58119
helios0722 <90462652+helios0722@users.noreply.github.com>
2015-09-28 14:08:00 -0700

    Component Updated - D:/Work/@/report-docx-generator-demo/tests/unit/pie-chart.test.ts

--- tests/unit/pie-chart.test.ts
+++ tests/unit/pie-chart.test.ts
@@ -1,16 +1,1 @@
-import { doesNotReject } from "assert";
-import { IReportResult } from "../../src/report-generator";
-import { Server } from "../../src/server";
-import { cloneObject } from "./test-utils";
-import testData from "./__mocks__/pie-chart/pie-chart.data";
-
-describe("Pie chart", () => {
-  it("Pie chart with style", async () => {
-    jest.setTimeout(30000);
-    const server = new Server();
-    const localtestData = cloneObject(testData);
-    const result: IReportResult = await server.test(localtestData);
-    expect(result.valid).toBeTruthy();
-    expect(result.content).toMatchSnapshot();
-  });
-});
+"Mon Sep 28 2015 14:08:00 GMT-0700 (Pacific Daylight Time)" 
